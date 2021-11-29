6d14fc7132b1f767a610afcd239046adf6647b25
MiShengLuLu <celina520524@outlook.com>
2021-11-29 21:15:57 +0800

    marketing router

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,1 +1,16 @@
-console.log('Marketing')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+function mount (el) {
+  ReactDOM.render(<App />, el)
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.getElementById('dev-marketing')
+  if (el) {
+    mount (el)
+  }
+}
+
+export { mount }
